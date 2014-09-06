a2077acc5a74545305ad1eb3862eb2996852cd7e
Flávio Schuindt <flavio.schuindt@gmail.com>
2014-09-05 22:18:20 -0300

    - Implementa sse

--- filter/sse.c
+++ filter/sse.c
@@ -19,7 +19,7 @@ void* do_sse(void *threadarg, int num_threads)
 	thread_pointer_data = (struct thread_data *)threadarg;
 
 	id = thread_pointer_data->thread_id;
-	imagem = thread_pointer_data->im1;
+	imagem = (float *)thread_pointer_data->im1;
 	res = thread_pointer_data->res;
 	w = thread_pointer_data->w;
 	h = thread_pointer_data->h;
@@ -27,6 +27,9 @@ void* do_sse(void *threadarg, int num_threads)
 	limit = 3*w*h / num_threads;
 	
 	for( idx = id*limit ; idx < (id+1)*limit ; idx++ ) {
+
+		if ( idx >= 3*w && idx < 3*w*(h-1) && idx % (3*w) != 0 && idx % (3*w-1) != 0)
+		{
 
 			elements[0] = imagem[idx-3];
 			elements[1] = imagem[idx-3*w];
@@ -44,21 +47,23 @@ void* do_sse(void *threadarg, int num_threads)
 			s = _mm_sub_ps(current_element_vector, elements_vector);
 			s = _mm_mul_ps(s, s);
 
-			aux = _mm_hadd_ps(s, s);      // sum horizontally
-			aux = _mm_hadd_ps(aux, aux);  // (NB: need to do this twice to sum all 4 elements)*/
-			/*aux = _mm_add_ps(s, _mm_movehl_ps(s, s));
-			__m128 sumxx = _mm_add_ss(aux, _mm_shuffle_ps(aux, aux, 1));*/
-			_mm_store_ss(&denominator_sum, aux); 
+			//aux = _mm_hadd_ps(s, s);      // sum horizontally
+			//aux = _mm_hadd_ps(aux, aux);  // (NB: need to do this twice to sum all 4 elements)*/
+			aux = _mm_add_ps(s, _mm_movehl_ps(s, s));
+			__m128 sumxx = _mm_add_ss(aux, _mm_shuffle_ps(aux, aux, 1));
+			_mm_store_ss(&denominator_sum, sumxx); 
 			denominator_sum += 1.0;
 
 			aux = _mm_mul_ps(s, elements_vector);
-			aux = _mm_hadd_ps(aux, aux);   // sum horizontally
-			aux = _mm_hadd_ps(aux, aux);  // (NB: need to do this twice to sum all 4 elements)
-			/*aux = _mm_add_ps(aux, _mm_movehl_ps(aux, aux));
-			sumxx = _mm_add_ss(aux, _mm_shuffle_ps(aux, aux, 1));*/
-			_mm_store_ss(&numerator_sum, aux);
+			//aux = _mm_hadd_ps(aux, aux);   // sum horizontally
+			//aux = _mm_hadd_ps(aux, aux);  // (NB: need to do this twice to sum all 4 elements)
+			aux = _mm_add_ps(aux, _mm_movehl_ps(s, s));
+			sumxx = _mm_add_ss(aux, _mm_shuffle_ps(aux, aux, 1));
+			_mm_store_ss(&numerator_sum, sumxx);
 			numerator_sum += imagem[ idx ]; 
 			
-			imagem[ idx ] = numerator_sum / sqrt(denominator_sum * denominator_sum);
+			res[ idx ] = numerator_sum / sqrt(denominator_sum * denominator_sum);
+
+		}
 	}
 }
